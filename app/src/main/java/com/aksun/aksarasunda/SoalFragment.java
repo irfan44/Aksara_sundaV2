d3e1ec001ee0b156b1237984bd36280caa851bec
Irfan Nurghiffari Muhajir <irfannmuhajir@gmail.com>
2021-12-27 06:32:12 +0700

    Ambil soal dari firebase

--- app/src/main/java/com/aksun/aksarasunda/SoalFragment.java
+++ app/src/main/java/com/aksun/aksarasunda/SoalFragment.java
@@ -1,64 +1,35 @@
 package com.aksun.aksarasunda;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SoalFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SoalFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SoalFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SoalFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SoalFragment newInstance(String param1, String param2) {
-        SoalFragment fragment = new SoalFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    CardView klikSoalHarian;
+    @Override
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_soal, container, false);
+        View view = inflater.inflate(R.layout.fragment_soal, container, false);
+        klikSoalHarian = view.findViewById(R.id.soalHarian);
+
+        klikSoalHarian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SoalHarianActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/aksun/aksarasunda/SoalHarianActivity.java
+++ app/src/main/java/com/aksun/aksarasunda/SoalHarianActivity.java
@@ -1,14 +1,107 @@
 package com.aksun.aksarasunda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class SoalHarianActivity extends AppCompatActivity {
+
+    TextView kuis;
+    RadioGroup rg;
+    RadioButton PilihanA, PilihanB, PilihanC;
+    Button next;
+    int nomor = 0;
+    public static int hasil, benar, salah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soal_harian);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("soal").document("soal_26dec21");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        List<String> soal = (List<String>) document.get("soal");
+                        List<String> pilihan = (List<String>) document.get("pilihan");
+                        List<String> jawaban = (List<String>) document.get("jawaban");
+
+                        kuis = (TextView) findViewById(R.id.kuis);
+                        rg = (RadioGroup) findViewById(R.id.pilihan);
+                        PilihanA = (RadioButton) findViewById(R.id.pilihanA);
+                        PilihanB = (RadioButton) findViewById(R.id.pilihanB);
+                        PilihanC = (RadioButton) findViewById(R.id.pilihanC);
+                        next = (Button) findViewById(R.id.next);
+
+                        rg.check(0);
+                        benar = 0;
+                        salah = 0;
+
+                        kuis.setText(soal.get(nomor));
+                        PilihanA.setText(pilihan.get(0));
+                        PilihanB.setText(pilihan.get(1));
+                        PilihanC.setText(pilihan.get(2));
+
+                        next.setOnClickListener(v -> {
+                            if (PilihanA.isChecked() || PilihanB.isChecked() || PilihanC.isChecked()) {
+
+                                RadioButton jawaban_user = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+                                String ambil_jawaban_user = jawaban_user.getText().toString();
+                                rg.check(0);
+                                if (ambil_jawaban_user.equalsIgnoreCase(jawaban.get(nomor))) benar++;
+                                else salah++;
+                                nomor++;
+                                if (nomor < soal.size()) {
+                                    kuis.setText(soal.get(nomor));
+                                    PilihanA.setText(pilihan.get((nomor * 3) + 0));
+                                    PilihanB.setText(pilihan.get((nomor * 3) + 1));
+                                    PilihanC.setText(pilihan.get((nomor * 3) + 2));
+
+                                } else {
+                                    hasil = benar * 20;
+                //                    Intent selesai = new Intent(getApplicationContext(), hasilsoalpg.class);
+                //                    startActivity(selesai);
+//                                    Toast.makeText(getApplicationContext(),"Kamu Jawab Dulu"+hasil,Toast.LENGTH_LONG).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"Kamu Jawab Dulu",Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Tidak mendapatkan data", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Gagal menjalankan query", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
+
+
 }
